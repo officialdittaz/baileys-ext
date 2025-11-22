fb13a22913e7d6228ba56579b38f6d2840cccfc1
naruyaizumi <sexystyle088@gmail.com>
2025-11-22 14:45:15 +0000

    style: auto-format with Prettier [skip-prettier]

--- lib/Socket/Client/websocket.d.ts
+++ lib/Socket/Client/websocket.d.ts
@@ -1,5 +1,5 @@
 // import WebSocket from 'ws';
-import { AbstractSocketClient } from './types.js';
+import { AbstractSocketClient } from "./types.js";
 export declare class WebSocketClient extends AbstractSocketClient {
     protected socket: WebSocket | null;
     get isOpen(): boolean;
